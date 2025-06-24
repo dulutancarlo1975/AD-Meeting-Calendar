e075b11903c7f235474010f2cefb4ce91acb9b7c
Carlo Dulutan <202310776@fit.edu.ph>
2025-06-24 08:18:50 -0700

    refactor(bootstrap.php)

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('UTILS_PATH', BASE_PATH . '/utils/');
+define('VENDORS_PATH', BASE_PATH . '/vendor/');
+define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 
 chdir(BASE_PATH);
