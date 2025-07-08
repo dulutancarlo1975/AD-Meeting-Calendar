f1884789b92f2b9fa9c96c088595bee99a82b491
Carlo Dulutan <202310776@fit.edu.ph>
2025-07-07 22:03:11 -0700

    feat: add dummy user data for testing purposes

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    [
+        'username' => 'john.smith',
+        'full_name' => 'John Smith',
+        'email' => 'john.smith@example.com',
+        'password' => 'p@ssW0rd1234',
+        'role' => 'designer'
+    ],
+    [
+        'username' => 'jane.doe',
+        'full_name' => 'Jane Doe',
+        'email' => 'jane.doe@example.com',
+        'password' => 's3cur3P@ss',
+        'role' => 'developer'
+    ],
+];
