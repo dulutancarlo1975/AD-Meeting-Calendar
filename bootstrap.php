adb14ce86b9ef68c65d8a7e143ab1529f3161170
Carlo Dulutan <202310776@fit.edu.ph>
2025-07-07 22:02:30 -0700

    refactor: improve code readability by adding comments for path definitions

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,11 @@
 <?php
+// Define important paths
 define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDORS_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+define('STATICDATAS_PATH', BASE_PATH . '/staticDatas/');
+define('DUMMIES_PATH', STATICDATAS_PATH . 'dummies/');
 
+// Change current working directory
 chdir(BASE_PATH);
